0ea4aa3496017bb3cd24745fbae1dd6a3ff9ce5b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-02-04 10:29:08 +0000

    Added integerToRoman function at script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ function getNumberInfo() {
     document.getElementById("numinfo").innerHTML = txt;
 
 }
+
+// Function to Convert Integer to Roman Numeral
+function integerToRoman(number) {
+    // Set Up Variables
+    let val=[1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    let sym=["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+    let i=12;
+    let result="";
+
+    while (number) {
+        let div = Math.floor(number/val[i]);
+        number %= val[i];
+
+        while (div) {
+            // Add the symbol to the result
+            result += sym[i];
+            div -= 1;
+        }
+        i -= 1;
+    }
+
+    // Return the result
+    return result;
+}
+
+// Function to Clear Form
